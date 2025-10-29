a73d020239146544ce75742739056064495c107b
Carsten Petersen <capetersen@ucsd.edu>
2025-10-29 16:17:20 -0700

    added equation screen MVC files

---
+++ src/screens/GameScreen/GameScreenModel.ts
@@ -0,0 +1,36 @@
+export class GameScreenModel {
+    private score: number = 0;
+
+    private currentExpression = {
+        fraction1: { numerator: 1, denominator: 2 },
+        fraction2: { numerator: 1, denominator: 4 },
+        operator: '+' as '+' | '-' | '*' | '/',
+    };
+    private answerChoices = [
+        { numerator: 3, denominator: 4 },  // Correct answer
+        { numerator: 1, denominator: 2 },
+        { numerator: 2, denominator: 3 },
+        { numerator: 1, denominator: 8 },
+    ];
+    private correctAnswerIndex = 0;
+
+    constructor() {
+        // Just dummy data for now
+    }
+
+    generateNewProblem(): void {
+        // TODO
+    }
+
+    checkAnswer(selectedIndex: number): boolean {
+        return selectedIndex === this.correctAnswerIndex;
+    }
+
+    incrementScore(): void {
+        this.score++;
+    }
+
+    getCurrentExpression() { return this.currentExpression; }
+    getAnswerChoices() { return this.answerChoices; }
+    getScore() { return this.score; }
+}
