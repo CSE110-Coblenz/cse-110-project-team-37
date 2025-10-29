e0ef1d85ffccdbbe47d932fdceba758efe942ef1
cargo22 <carlosegomez1738@gmail.com>
2025-10-29 15:54:19 -0700

    Basic main menu functions working. File structure copied from Lab 3

---
+++ src/types.ts
@@ -0,0 +1,38 @@
+// stripped from lab 3. The idea is that as we add screens, we can update this file, 
+// so that switching screens becomes easy.
+import type { Group } from "konva/lib/Group";
+
+// interface for view
+export interface View {
+	getGroup(): Group;
+	show(): void;
+	hide(): void;
+}
+
+/**
+ * Screen types for navigation
+ *
+ * - "menu": Main menu screen
+ * - "game": Gameplay screen
+ * - "help": Tutorial Help screen
+ */
+export type Screen =
+	| { type: "menu" }
+	| { type: "game" }
+	| { type: "help"};
+
+export abstract class ScreenController {
+	abstract getView(): View;
+
+	show(): void {
+		this.getView().show();
+	}
+
+	hide(): void {
+		this.getView().hide();
+	}
+}
+
+export interface ScreenSwitcher {
+	switchToScreen(screen: Screen): void;
+}
