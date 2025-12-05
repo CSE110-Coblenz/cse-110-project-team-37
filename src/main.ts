4957490cc147b77e0aec83f9f7e626ce33351b55
unknown <jwilkens@ucsd.edu>
2025-12-05 01:46:46 -0800

    new lint fix

--- src/main.ts
+++ src/main.ts
@@ -377,4 +377,4 @@ class App implements ScreenSwitcher {
 new App("app");
 
 // Start background music immediately (autoplay hack handled inside)
-const bgm = new MusicManager("/bg-music.mp3");
+void new MusicManager("/bg-music.mp3");
